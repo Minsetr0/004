556fc97258223c81a26ac8491ca2617c49c72311
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-20 22:14:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int[] numbers = new int[0];
+string commandToExit = "exit";
+string commandToSum = "sum";
+int sumAllNumbers = 0;
+int forTheAmount;
+string userInput = null;
+
+while (userInput != commandToExit)
+{
+    Console.WriteLine("Введите число. Если хотите увидеть сумму всех введённых вами чисел, введите sum, если хотите выйти, введите exit.");
+    userInput = Console.ReadLine();
+
+    if (userInput == commandToSum)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sumAllNumbers += numbers[i];
+        }
+
+        Console.WriteLine("Сумма всех ваших чисел равна " + sumAllNumbers);
+        sumAllNumbers = 0;
+    }
+    else if (userInput != commandToSum && userInput != commandToExit)
+    {
+        forTheAmount = Convert.ToInt32(userInput);
+        int[] tempNumbers = new int[numbers.Length + 1];
+
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            tempNumbers[j] = numbers[j];
+        }
+
+        tempNumbers[tempNumbers.Length - 1] = forTheAmount;
+        numbers = tempNumbers;
+    }
+}
